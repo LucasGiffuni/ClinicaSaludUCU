eb163f8583878ca90f36179861b16460494ecf97
Lucas Giffuni <lucasgiffuni@gmail.com>
2023-10-28 14:35:24 -0300

    Initial Commit

---
+++ Backend/clinica_ucu/src/main/java/com/example/clinica_ucu/ClinicaUcuApplication.java
@@ -0,0 +1,13 @@
+package com.example.clinica_ucu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaUcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaUcuApplication.class, args);
+	}
+
+}
---
+++ Backend/clinica_ucu/src/test/java/com/example/clinica_ucu/ClinicaUcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clinica_ucu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaUcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
