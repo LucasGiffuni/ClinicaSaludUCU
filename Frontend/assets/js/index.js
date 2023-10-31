df47d0516309c86d944a5fdda8f89d13370530f9
RobottiFranco <franco.robotti0@gmail.com>
2023-10-31 01:06:39 -0300

    add security frontend and verify data

--- Frontend/assets/js/index.js
+++ Frontend/assets/js/index.js
@@ -1,30 +1,37 @@
-const form_login_register = document.querySelector("#forms-login-register");
+const form_css = document.querySelector("#forms");
 const form_login_css = document.querySelector("#login-form");
 const form_register_css = document.querySelector("#register-form");
-const background_login = document.querySelector("#button-inicio-sesion");
-const background_register = document.querySelector("#button-register");
+const background_login_css = document.querySelector("#background-login-btn");
+const background_register_css = document.querySelector(
+  "#background-register-btn"
+);
 
 const register_form = document.getElementById("register-form");
 const login_form = document.getElementById("login-form");
 
+/* register */
 let nameInput = document.getElementById("name");
 let lastNameInput = document.getElementById("lastName");
 let birthdateInput = document.getElementById("birthdate");
 let addressInput = document.getElementById("address");
 let phoneInput = document.getElementById("phone");
 let ciInput = document.getElementById("ci");
-let passwordInput = document.getElementById("password");
+let passwordInput = document.getElementById("password-register");
 let emailInput = document.getElementById("email");
-let loginIdInput = document.getElementById("loginIdRegister");
+let loginIdInput = document.getElementById("loginId-Register");
+
+/* login */
+let loginIdInputLogin = document.getElementById("loginId-login");
+let passwordInputLogin = document.getElementById("password-login");
 
 const showPasswordBtn = document.getElementById("showPasswordBtn");
 
 /* functions */
 register_form.addEventListener("submit", registerForm);
-/* login_form.addEventListener("submit", loginForm); */
+login_form.addEventListener("submit", loginForm);
 
-background_login.addEventListener("click", inicioSesionSwap);
-background_register.addEventListener("click", registerSwap);
+background_login_css.addEventListener("click", inicioSesionSwap);
+background_register_css.addEventListener("click", registerSwap);
 window.addEventListener("resize", redimention);
 showPasswordBtn.addEventListener("click", showPassword);
 
@@ -34,45 +41,45 @@ redimention();
 function registerSwap() {
   if (window.innerWidth > 850) {
     form_register_css.style.display = "block";
-    form_login_register.style.left = "410px";
+    form_css.style.left = "410px";
     form_login_css.style.display = "none";
-    background_register.style.opacity = "0";
-    background_login.style.opacity = "1";
+    background_register_css.style.opacity = "0";
+    background_login_css.style.opacity = "1";
   } else {
     form_register_css.style.display = "block";
-    form_login_register.style.left = "0px";
+    form_css.style.left = "0px";
     form_login_css.style.display = "none";
-    background_register.style.display = "none";
-    background_login.style.display = "block";
-    background_login.style.opacity = "1";
+    background_register_css.style.display = "none";
+    background_login_css.style.display = "block";
+    background_login_css.style.opacity = "1";
   }
 }
 
 function inicioSesionSwap() {
   if (window.innerWidth > 850) {
     form_login_css.style.display = "block";
-    form_login_register.style.left = "10px";
+    form_css.style.left = "10px";
     form_register_css.style.display = "none";
-    background_register.style.opacity = "1";
-    background_login.style.opacity = "0";
+    background_register_css.style.opacity = "1";
+    background_login_css.style.opacity = "0";
   } else {
     form_login_css.style.display = "block";
-    form_login_register.style.left = "0px";
+    form_css.style.left = "0px";
     form_register_css.style.display = "none";
-    background_login.style.display = "none";
+    background_login_css.style.display = "none";
   }
 }
 
 function redimention() {
   if (window.innerWidth > 850) {
-    background_register.style.display = "block";
-    background_login.style.display = "block";
+    background_register_css.style.display = "block";
+    background_login_css.style.display = "block";
   } else {
-    background_register.style.display = "block";
-    background_register.style.opacity = "1";
-    background_login.style.display = "none";
+    background_register_css.style.display = "block";
+    background_register_css.style.opacity = "1";
+    background_login_css.style.display = "none";
     form_login_css.style.display = "block";
-    form_login_register.style.left = "0px";
+    form_css.style.left = "0px";
     form_register_css.style.display = "none";
   }
 }
@@ -86,65 +93,85 @@ function showPassword() {
 function isValidEmail(email) {
   const regexEmail =
     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+  alert("el email " + regexEmail.test(email));
   return regexEmail.test(email);
 }
 
 function isValidPassword(password) {
   const regexPassword = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+  alert("la contrasena " + regexPassword.test(password));
   return regexPassword.test(password);
 }
 
 function isValidNumeric(value) {
   const regexNumeric = /^[0-9]+$/;
+  alert("el numero " + regexNumeric.test(value));
   return regexNumeric.test(value);
 }
 
-function verifyData() {
-  const isValidEmailInput = isValidEmail(emailInput.value);
-  const isValidPasswordInput = isValidPassword(passwordInput.value);
-  const isValidLoginIdInput = isValidNumeric(loginIdInput.value);
-  const isValidCiInput = isValidNumeric(ciInput.value);
+function verifyData(value) {
+  if (value === "register") {
+    const isValidEmailInput = isValidEmail(emailInput.value);
+    const isValidPasswordInput = isValidPassword(passwordInput.value);
+    const isValidLoginIdInput = isValidNumeric(loginIdInput.value);
+    const isValidCiInput = isValidNumeric(ciInput.value);
+    const isValidPhone = isValidNumeric(phoneInput.value);
 
-  if (
-    nameInput.value.trim() !== "" &&
-    lastNameInput.value.trim() !== "" &&
-    birthdateInput.value.trim() !== "" &&
-    addressInput.value.trim() !== "" &&
-    phoneInput.value.trim() !== "" &&
-    ciInput.value.trim() !== "" &&
-    emailInput.value.trim() !== "" &&
-    passwordInput.value.trim() !== "" &&
-    loginIdInput.value.trim() !== "" &&
-    isValidEmailInput &&
-    isValidPasswordInput &&
-    isValidLoginIdInput &&
-    isValidCiInput
-  ) {
-    return true;
+    if (
+      nameInput.value.trim() !== "" &&
+      lastNameInput.value.trim() !== "" &&
+      addressInput.value.trim() !== "" &&
+      phoneInput.value.trim() !== "" &&
+      ciInput.value.trim() !== "" &&
+      emailInput.value.trim() !== "" &&
+      passwordInput.value.trim() !== "" &&
+      loginIdInput.value.trim() !== "" &&
+      isValidEmailInput &&
+      isValidPasswordInput &&
+      isValidLoginIdInput &&
+      isValidCiInput &&
+      isValidPhone
+    ) {
+      return true;
+    } else {
+      alert("Por favor, completa todos los campos correctamente.");
+      return false;
+    }
   } else {
-    alert("Por favor, completa todos los campos correctamente.");
-    return false;
+    const isValidLoginIdInputLogin = isValidNumeric(loginIdInputLogin.value);
+    const isValidPasswordInputLogin = isValidPassword(passwordInputLogin.value);
+
+    if (
+      passwordInputLogin.value.trim() !== "" &&
+      loginIdInputLogin.value.trim() !== "" &&
+      isValidLoginIdInputLogin &&
+      isValidPasswordInputLogin
+    ) {
+      return true;
+    } else {
+      alert("Por favor, completa todos los campos correctamente.");
+      return false;
+    }
   }
 }
 
 function registerForm() {
-  if (verifyData) {
-    // Enviar solicitud POST al backend
+  if (verifyData("register")) {
     fetch("/backend/endpoint_de_registro", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        name: nameInput.value,
-        lastName: lastNameInput.value,
+        name: nameInput.value.trim(),
+        lastName: lastNameInput.value.trim(),
         birthdate: birthdateInput.value,
         address: addressInput.value,
-        phone: parseInt(phoneInput.value),
-        ci: parseInt(ciInput.value),
-        email: emailInput.value,
-        password: passwordInput.value,
-        loginId: parseInt(loginIdInput.value),
+        phone: parseInt(phoneInput.value.trim()),
+        ci: parseInt(ciInput.value.trim()),
+        email: emailInput.value.trim(),
+        password: passwordInput.value.trim(),
+        loginId: parseInt(loginIdInput.value.trim()),
       }),
     })
       .then((response) => response.json())
@@ -158,24 +185,17 @@ function registerForm() {
   }
 }
 
-function loginForm() {}
-// Código para manejar las solicitudes POST desde el formulario
-document
-  .getElementById("login-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Evitar que el formulario se envíe de forma predeterminada
-
-    // Obtener datos del formulario
-    const loginId = document.getElementById("loginId").value;
-    const password = document.getElementById("LoginPassword").value;
-
-    // Enviar solicitud POST al backend
+function loginForm() {
+  if (verifyData("login")) {
     fetch("/backend/endpoint_de_login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ loginId: loginId, password: password }),
+      body: JSON.stringify({
+        loginId: parseInt(loginIdInputLogin.value.trim()),
+        password: passwordInputLogin.value.trim(),
+      }),
     })
       .then((response) => response.json())
       .then((data) => {
@@ -185,4 +205,5 @@ document
       .catch((error) => {
         console.error("Error:", error);
       });
-  });
+  }
+}
