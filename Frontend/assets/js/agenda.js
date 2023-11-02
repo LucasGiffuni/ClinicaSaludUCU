68ceb5e7c6eda3a8d217f98ab8ee0acbfeb517f7
RobottiFranco <franco.robotti0@gmail.com>
2023-11-02 19:16:50 -0300

    schedule

--- Frontend/assets/js/agenda.js
+++ Frontend/assets/js/agenda.js
@@ -1,16 +1,84 @@
-document.addEventListener("DOMContentLoaded", function() {
-    flatpickr("#calendar", {
-        minDate: "2023/11/01",
-        maxDate: "2023/11/18",
-        dateFormat: "d/m/y" 
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  let agendaForm = document.getElementById("agendaForm");
+  let submitButton = document.getElementById("submitButton");
+  let formularioEnviado = false;
 
-    document.getElementById("agendaForm").addEventListener("submit", function(event) {
-        event.preventDefault();
-        const selectedDate = document.getElementById("calendar").value;
-        const selectedHour = document.getElementById("hour").value;
-        // Aquí puedes hacer lo que desees con la fecha y la hora seleccionadas
-        console.log("Fecha seleccionada: " + selectedDate);
-        console.log("Hora seleccionada: " + selectedHour);
-    });
+  const bookedSlots = {}; // Objeto para realizar un seguimiento de las fechas y horas agendadas
+
+  agendaForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    if (!formularioEnviado) {
+      schedule();
+      formAlreadySent();
+    } else {
+      alert("¡Ya te haz agendado!");
+    }
+  });
+
+  function formAlreadySent() {
+    formularioEnviado = true;
+    submitButton.disabled = true; // Deshabilitar el botón después de enviar el formulario
+  }
+
+  flatpickr("#calendar", {
+    minDate: "2023/11/01",
+    maxDate: "2023/11/18",
+    dateFormat: "d/m/y",
+    disable: [
+      function (date) {
+        const dateString = date.toISOString().split("T")[0];
+        return bookedSlots[dateString];
+      },
+    ],
+  });
+
+  // Agrega event listeners para detectar cambios en la fecha y la hora
+  document
+    .getElementById("calendar")
+    .addEventListener("change", enableSubmitButton);
+  document
+    .getElementById("hour")
+    .addEventListener("change", enableSubmitButton);
+
+  function enableSubmitButton() {
+    // Habilita el botón de submit si se ha seleccionado una fecha y una hora
+    const selectedDate = document.getElementById("calendar").value;
+    const selectedHour = document.getElementById("hour").value;
+    if (selectedDate && selectedHour) {
+      submitButton.disabled = false;
+    } else {
+      submitButton.disabled = true;
+    }
+  }
+
+  function schedule() {
+    const selectedDate = document.getElementById("calendar").value;
+    const selectedHour = document.getElementById("hour").value;
+
+    // Verificar si la hora está disponible
+    if (!bookedSlots[selectedDate]) {
+      bookedSlots[selectedDate] = [];
+    }
+
+    if (!bookedSlots[selectedDate].includes(selectedHour)) {
+      bookedSlots[selectedDate].push(selectedHour);
+
+      // Eliminar la opción del campo de selección de hora
+      const hourSelect = document.getElementById("hour");
+      const optionToRemove = hourSelect.querySelector(
+        `option[value="${selectedHour}"]`
+      );
+      hourSelect.removeChild(optionToRemove);
+
+      alert(
+        "Fecha seleccionada: " +
+          selectedDate +
+          " Hora seleccionada: " +
+          selectedHour
+      );
+    } else {
+      alert("¡Lo siento! Esta hora ya está agendada.");
+    }
+  }
 });
