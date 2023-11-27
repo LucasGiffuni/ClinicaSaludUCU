4171b5c87548b99829649fe772808dfeb6abc129
Franco Robotti <108959274+RobottiFranco@users.noreply.github.com>
2023-11-27 14:58:37 -0300

    Update CLINICA DML - example data.sql

--- BDD/CLINICA DML.sql
+++ BDD/CLINICA DML.sql
@@ -60,3 +60,21 @@ CREATE TABLE Cupos
 INSERT INTO Periodos_Actualizacion (Año, Semestre, Fch_Inicio, Fch_Fin)
 VALUES
   (2023, '1', '2023-11-01', '2023-11-15');
+
+INSERT INTO Logins (LogId, Password)
+VALUES
+  ('juanperez', 'contraseña123'),
+  ('mariagomez', 'segura456'),
+  ('analopez', 'clave789'),
+  ('pedroramirez', 'secretaABC'),
+  ('lauramartinez', 'mipass123'),
+  ('carlosgarcia', 'miclave456'),
+  ('sofiadiaz', 'contrasenaXYZ');
+
+INSERT INTO Funcionarios (Ci, Nombre, Apellido, Fch_Nacimiento, Direccion, Telefono, Email, LogId)
+VALUES
+  (11112222, 'Ana', 'López', '1988-07-10', 'Calle Sur 789', 012, 'ana@example.com', 'analopez'),
+  (33334444, 'Pedro', 'Ramírez', '1995-03-25', 'Avenida Norte 567', 023, 'pedro@example.com', 'pedroramirez'),
+  (55556666, 'Laura', 'Martínez', '1980-11-02', 'Calle Este 234', 034, 'laura@example.com', 'lauramartinez'),
+  (77778888, 'Carlos', 'García', '1992-09-18', 'Avenida Oeste 890', 045, 'carlos@example.com', 'carlosgarcia'),
+  (99990000, 'Sofía', 'Díaz', '1987-12-07', 'Calle Central 456', 056, 'sofia@example.com', 'sofiadiaz');
